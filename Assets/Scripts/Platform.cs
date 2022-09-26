e253a4be67051a5731ffbac5fe3c8e7314fa93e5
Ilya <kamad0tanjiro@outlook.com>
2022-09-26 18:13:38 +0300

    Update Platform.cs
    Made a movement through the coroutine

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -1,37 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
     [SerializeField] private Transform _path;
     [SerializeField] private float _speed;
+    [SerializeField] private float _delaydLifting = 5f;
 
-    private float _delay = 5f;
     private Transform[] _points;
-    private Transform target;
+    private Transform _target;
     private int _currentPoint;
     private float _time;
+    private Coroutine _move;
 
     private void Start()
     {
         _points = new Transform[_path.childCount];
-
+    
         for (int i = 0; i < _path.childCount; i++)
         {
             _points[i] = _path.GetChild(i);
         }
+        
+        StartCoroutine(Move());
     }
-
-    private void Update()
+    
+    private IEnumerator Move()
     {
         _time += Time.deltaTime;
-        target = _points[_currentPoint];
-        transform.position = Vector3.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+        _target = _points[_currentPoint];
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
 
-        if (_time > _delay)
+        if (_time > _delaydLifting)
         {
             _time = 0f;
             
-            if (transform.position == target.position)
+            if (transform.position == _target.position)
             {
                 _currentPoint++;
 
@@ -41,5 +45,8 @@ public class Platform : MonoBehaviour
                 }
             }
         }
+
+        yield return null;
+        StartCoroutine(Move());
     }
 }
