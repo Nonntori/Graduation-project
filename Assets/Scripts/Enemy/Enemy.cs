c3154ff19fb042f8bca4e96e79fa125b5e46ac8e
Ilya <kamad0tanjiro@outlook.com>
2022-09-26 20:26:25 +0300

    Update Enemy.cs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,15 +7,14 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int _health = 100;
     [SerializeField] private Player _target;
     
-    public event UnityAction Damaged;
-    public event UnityAction Died;
-
-    public Player Targer => _target;
-    public int Health => _health;
-    
     private Collider2D _collider2D;
     private Rigidbody2D _rigidbody2D;
+    
+    public Player Targer => _target;
 
+    public event UnityAction Damaged;
+    public event UnityAction Died;
+    
     private void Start()
     {
         _collider2D = GetComponent<Collider2D>();
